60cc07c757784380fd7ca39dc1556b7d7e7ae5e7
rvth09 <107382153+rvth09@users.noreply.github.com>
2023-02-28 17:03:50 +0700

    mulai kerja halaman login

---
+++ app/src/main/java/com/jitusolution/native160420076/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jitusolution.native160420076
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Todo: membuat halaman login
+    }
+}
