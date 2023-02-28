003cc4942b545869f4173701c6c08cd7d8eca0f5
rvth09 <107382153+rvth09@users.noreply.github.com>
2023-02-28 17:33:50 +0700

    tambah activity change password

---
+++ app/src/main/java/com/jitusolution/native160420076/ChangePasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.jitusolution.native160420076
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+
+        //Change password
+    }
+}
